4e102e490287a9a424350591d40f65710ddeb0dd
Italo Pessoa <italo.pessoa@hotmail.com>
2017-09-22 22:16:03 -0300

    setup EF Core Migrations

---
+++ BitcoinShow.Web/BitcoinShowDbContextFactory.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using BitcoinShow.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BitcoinShow.Web
+{
+    public class BitcoinShowDbContextFactory : IDesignTimeDbContextFactory<BitcoinShowDBContext>
+    {
+        public BitcoinShowDBContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<BitcoinShowDBContext>();
+
+            var connectionString = configuration.GetConnectionString("SqlServer");
+            builder.UseSqlite(connectionString);
+
+            return new BitcoinShowDBContext(builder.Options);
+        }
+    }
+}
---
+++ BitcoinShow.Web/Migrations/20170923010725_InitialMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BitcoinShow.Web.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
