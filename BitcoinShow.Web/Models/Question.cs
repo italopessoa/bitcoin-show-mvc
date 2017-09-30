56e1644d9ff41cc7c21b167f7f0eb1bf4d7709b7
Italo Pessoa <italo.pessoa@hotmail.com>
2017-09-29 21:34:09 -0300

    #5 add details to Option Entity :construction:

--- BitcoinShow.Web/Models/Question.cs
+++ BitcoinShow.Web/Models/Question.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -5,17 +6,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace BitcoinShow.Web.Models
 {
     /// <summary>
-    ///     Question
+    ///       Question
     /// </summary>
     [Table("Question", Schema = "bs")]
     public class Question
     {
         /// <summary>
-        ///     Default constructor
-        /// </summary>
-        public Question()
+        ///     Constructor
+        /// </summary> 
+        /// <param name="title"> Question title. </param>
+        /// <param name="answer"> Question answer. </param>
+        /// <param name="options"> Question options. </param>
+        public Question(string title, Option answer, List<Option> options)
         {
+            this.Title = title;
+            this.Answer = answer;
+            this.Options = options;
         }
+        
         /// <summary>
         ///     Question Id
         /// </summary>
@@ -25,20 +33,77 @@ namespace BitcoinShow.Web.Models
         /// <summary>
         ///     Question Title
         /// </summary>
-        [Required()]
-        [MaxLength(150)]
+        [Required]
+        [MaxLength(200)]
         public string Title { get; set; }
 
         /// <summary>
         ///     Question answer
         /// </summary>
-        // [Required]
-        // public Option Answer { get; set; }
+        [Required]
+        public Option Answer { get; set; }
 
         /// <summary>
         ///     Question options
         /// </summary>
         [Required]
         public List<Option> Options { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Question question = (Question)obj;
+
+            if((Options == null && question.Options != null)
+                || (Options != null && question.Options == null)
+                || (Options.Count != question.Options.Count)
+                )
+            {
+                return false;
+            }
+
+            if(Options != null)
+            {
+                for (int i = 0; i < Options.Count; i++)
+                {
+                    if(!Options[i].Equals(question.Options[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return (Id == question.Id) && (Title.Equals(question.Title)) 
+                && Answer.Equals(question.Answer);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + Id.GetHashCode();
+                if (!string.IsNullOrEmpty(Title))
+                {
+                    hash = hash * 23 + Title.GetHashCode();
+                }
+                if(Answer != null)
+                {
+                    hash = hash * 23 + Answer.GetHashCode();
+                }
+                if(Options != null)
+                {
+                    Options.ForEach(option => 
+                    {
+                        hash = hash * 23 + option.GetHashCode();
+                    });
+                }
+                return hash;
+            }
+        }
     }
 }
